60c51cca954bd66bb8eddfa3eb9f15af511382fd
Edwin Hurtado <thaednevol@gmail.com>
2014-09-21 21:58:55 -0500

    Código QR Funcionando

--- radarinmobiliario/src/co/encefalo/radarinmobiliario/BuscarActivity.java
+++ radarinmobiliario/src/co/encefalo/radarinmobiliario/BuscarActivity.java
@@ -3,8 +3,12 @@ package co.encefalo.radarinmobiliario;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class BuscarActivity extends Activity {
 	
@@ -29,6 +33,45 @@ public class BuscarActivity extends Activity {
 		Intent intent = new Intent("co.encefalo.radarinmobiliario.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
         startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+	    super.onActivityResult(requestCode, resultCode, data);
+	    
+	    if (data.hasExtra("displayContents")){
+	    	traer_datos(data.getStringExtra("displayContents"));
+	    }
+	    
+	    Log.d("onActivityResult", requestCode+" "+resultCode+" "+data.toString());
+	}
+
+	private void traer_datos(String stringExtra) {
+		ConexionInternet ci = new ConexionInternet(this);
+		Handler puente = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				if (msg.what == 0) {
+					conseguir_inmueble(msg.obj.toString());
+				} else if (msg.what == 2){
+					Toast.makeText(getApplicationContext(), "Conéctese a internet", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Toast.makeText(getApplicationContext(), "Error desconocido", Toast.LENGTH_SHORT).show();
+				}
+			}
+		};
+		ci.setPuente(puente);
+		ci.setString_url(Propiedades.URL_QR+stringExtra);
+		ci.execute();
+	}
+
+	protected void conseguir_inmueble(String string) {
+		JSONInmuebleParser jip = new JSONInmuebleParser(string);
+		Intent i = new Intent(getBaseContext(), ListarInmueblesActivity.class);
+		i.putParcelableArrayListExtra(Propiedades.ARRAYPROPIEDADES, jip.getInmuebles());
+		startActivity(i);
 	}
 
 }
--- radarinmobiliario/src/co/encefalo/radarinmobiliario/Propiedades.java
+++ radarinmobiliario/src/co/encefalo/radarinmobiliario/Propiedades.java
@@ -6,6 +6,7 @@ public class Propiedades {
 	public static final int GET = 0;
 	public static final int POST = 1;
 	public static final String URL = "http://encefalo.co/radar/endpoint_services/propertieslist";//"http://encefalo.co/radar/endpoint_services/properties";
+	public static final String URL_QR = "http://encefalo.co/radar/endpoint_services/properties?nid=";
 	public static final String IMAGES = "images";
 	public static final String NUMVIEWS = "numviews";
 	public static final String ARRAYPROPIEDADES = "arraypropiedades";
