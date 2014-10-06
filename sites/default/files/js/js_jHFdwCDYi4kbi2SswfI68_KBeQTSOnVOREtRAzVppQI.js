89716e6bea60d1b298bae23a352c67adeaf49ba5
Edwin Hernandez C <ehernandez@micrositios.net>
2014-10-06 17:22:13 -0500

    Actualizacion de seguridad

---
+++ sites/default/files/js/js_jHFdwCDYi4kbi2SswfI68_KBeQTSOnVOREtRAzVppQI.js
@@ -0,0 +1,119 @@
+;
+(function ($) {
+
+Drupal.behaviors.initColorbox = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+
+    if (settings.colorbox.mobiledetect && window.matchMedia) {
+      // Disable Colorbox for small screens.
+      var mq = window.matchMedia("(max-device-width: " + settings.colorbox.mobiledevicewidth + ")");
+      if (mq.matches) {
+        return;
+      }
+    }
+
+    $('.colorbox', context)
+      .once('init-colorbox')
+      .colorbox(settings.colorbox);
+
+    $(context).bind('cbox_complete', function () {
+      Drupal.attachBehaviors('#cboxLoadedContent');
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.initColorboxDefaultStyle = {
+  attach: function (context, settings) {
+    $(context).bind('cbox_complete', function () {
+      // Only run if there is a title.
+      if ($('#cboxTitle:empty', context).length == false) {
+        $('#cboxLoadedContent img', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideDown();
+        });
+        $('#cboxOverlay', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideUp();
+        });
+      }
+      else {
+        $('#cboxTitle', context).hide();
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Attaches double-click behavior to toggle full path of Krumo elements.
+ */
+Drupal.behaviors.devel = {
+  attach: function (context, settings) {
+
+    // Add hint to footnote
+    $('.krumo-footnote .krumo-call').once().before('<img style="vertical-align: middle;" title="Click to expand. Double-click to show path." src="' + settings.basePath + 'misc/help.png"/>');
+
+    var krumo_name = [];
+    var krumo_type = [];
+
+    function krumo_traverse(el) {
+      krumo_name.push($(el).html());
+      krumo_type.push($(el).siblings('em').html().match(/\w*/)[0]);
+
+      if ($(el).closest('.krumo-nest').length > 0) {
+        krumo_traverse($(el).closest('.krumo-nest').prev().find('.krumo-name'));
+      }
+    }
+
+    $('.krumo-child > div:first-child', context).dblclick(
+      function(e) {
+        if ($(this).find('> .krumo-php-path').length > 0) {
+          // Remove path if shown.
+          $(this).find('> .krumo-php-path').remove();
+        }
+        else {
+          // Get elements.
+          krumo_traverse($(this).find('> a.krumo-name'));
+
+          // Create path.
+          var krumo_path_string = '';
+          for (var i = krumo_name.length - 1; i >= 0; --i) {
+            // Start element.
+            if ((krumo_name.length - 1) == i)
+              krumo_path_string += '$' + krumo_name[i];
+
+            if (typeof krumo_name[(i-1)] !== 'undefined') {
+              if (krumo_type[i] == 'Array') {
+                krumo_path_string += "[";
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += krumo_name[(i-1)];
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += "]";
+              }
+              if (krumo_type[i] == 'Object')
+                krumo_path_string += '->' + krumo_name[(i-1)];
+            }
+          }
+          $(this).append('<div class="krumo-php-path" style="font-family: Courier, monospace; font-weight: bold;">' + krumo_path_string + '</div>');
+
+          // Reset arrays.
+          krumo_name = [];
+          krumo_type = [];
+        }
+      }
+    );
+  }
+};
+
+})(jQuery);
+;
